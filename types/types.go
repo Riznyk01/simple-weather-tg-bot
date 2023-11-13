6b73c2cc076f1d7722272d0b7594fa3d9c0919c9
Oleksandr Riznyk <OleksandrRiznyk01@gmail.com>
2023-11-13 10:19:28 +0200

    Remove obsolete types in types.go

--- types/types.go
+++ types/types.go
@@ -1,39 +1,5 @@
 package types
 
-type Update struct {
-	UpdateId int     `json:"update_id"`
-	Message  Message `json:"message"`
-}
-
-type Message struct {
-	Chat Chat   `json:"chat"`
-	Text string `json:"text"`
-}
-
-type Chat struct {
-	ChatId int `json:"id"`
-}
-
-type RestResponse struct {
-	Result []Update `json:"result"`
-}
-
-type RespMessage struct {
-	ChatId int    `json:"chat_id"`
-	Text   string `json:"text"`
-}
-
-type Geocoding struct {
-	//Name string `json:"name"`
-	//LocalNames struct {
-	//	Iu          string `json:"iu,omitempty"`
-	//} `json:"local_names,omitempty"`
-	Lat float64 `json:"lat"`
-	Lon float64 `json:"lon"`
-	//Country string  `json:"country"`
-	//State   string  `json:"state"`
-}
-
 type WeatherResponse struct {
 	Coord struct {
 		Lon float64 `json:"lon"`
