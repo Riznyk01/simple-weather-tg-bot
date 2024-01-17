77b33624ad91da9153048a0058dd893117a0b652
Oleksandr Riznyk <OleksandrRiznyk01@gmail.com>
2024-01-17 13:28:50 +0200

    Added comments to handle update functions

--- utils/converting.go
+++ utils/converting.go
@@ -5,25 +5,16 @@ import (
 	"time"
 )
 
+// PressureConverting converting pressure to HPaToInHg.
 func PressureConverting(hPa float64, metric bool) float64 {
 	if metric {
 		//HPaToMmHg
 		return hPa * 0.750061561303
 	}
-	//HPaToInHg
 	return hPa * 0.0295299830714
 }
-func TimeStampToHuman(timeStamp, timezone int, format string) string {
-	location := time.FixedZone("Custom Timezone", timezone)
-	timeValue := time.Unix(int64(timeStamp), 0).In(location)
-	return timeValue.Format(format)
-}
-func TemperatureConverting(temp float64, metric bool) int16 {
-	if metric {
-		return int16(math.Round(temp))
-	}
-	return int16(math.Round((temp-273.15)*(9/5) + 32))
-}
+
+// TimeStampToInfo converts a Unix timestamp to specific information based on the provided info type.
 func TimeStampToInfo(timeStamp, timezone int, infoType string) string {
 	location := time.FixedZone("Custom Timezone", timezone)
 	timeValue := time.Unix(int64(timeStamp), 0).In(location)
@@ -37,9 +28,28 @@ func TimeStampToInfo(timeStamp, timezone int, infoType string) string {
 		return "Invalid info type"
 	}
 }
+
+// TimeStampToHuman converts a timestamp to a human-readable string representation of time.
+func TimeStampToHuman(timeStamp, timezone int, format string) string {
+	location := time.FixedZone("Custom Timezone", timezone)
+	timeValue := time.Unix(int64(timeStamp), 0).In(location)
+	return timeValue.Format(format)
+}
+
+// TemperatureConverting converts temperature from Kelvin to Fahrenheit and rounds to the nearest integer.
+func TemperatureConverting(temp float64, metric bool) int16 {
+	if metric {
+		return int16(math.Round(temp))
+	}
+	return int16(math.Round((temp-273.15)*(9/5) + 32))
+}
+
+// ToMilesPerHour converting m/s to mph.
 func ToMilesPerHour(metersPerSecond float64) float64 {
 	return metersPerSecond * 2.23694
 }
+
+// DegreesToDirectionIcon add icon with wind direction.
 func DegreesToDirectionIcon(degrees float64) string {
 	if degrees >= 337.5 || degrees < 22.5 {
 		return "⬆️"
